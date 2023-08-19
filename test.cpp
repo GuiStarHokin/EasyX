7d17a43c797b505ccf15ae476ec3b456d63a8e9f
hokin <jian.hee@qq.com>
2023-08-19 20:29:39 +0800

    添加项目文件。

---
+++ test.cpp
@@ -0,0 +1,13 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <graphics.h>
+//#include <conio.h>
+//
+//int main()
+//{
+//	initgraph(640, 480);
+//	setlinecolor(RED);
+//	circle(200, 200, 100);
+//	_getch();
+//	closegraph();
+//	return 0;
+//}
